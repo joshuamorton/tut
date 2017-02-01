c724bc148fd098db1f430e623ed4cd40069561f2
Joshua Morton <joshua.morton13@gmail.com>
2017-02-01 00:00:15 -0500

    Swapped to use a specialized mock.
    This function only mocks io outside the given directory, meaning
    we can worry only about global io.

--- test/test_tut_cli.py
+++ test/test_tut_cli.py
@@ -1,56 +1,45 @@
-import unittest
 import os
 import toml
+import unittest
 from unittest import mock
 
 
 from click.testing import CliRunner
 import tut
+import test.utils
 
 class TutCLITest(unittest.TestCase):
     def test_basic_config(self):
         runner = CliRunner()
         with open('test/tutfiles/default.global') as tutfile:
-            m = mock.mock_open(read_data=tutfile.read())
-            mock_response = mock.MagicMock()
-            mock_response.text = 'mock_gitignore_data'
-            r = mock.MagicMock(return_value=mock_response)
+            data = tutfile.read()
+        mock_response = mock.MagicMock()
+        mock_response.text = 'mock_gitignore_data'
+        r = mock.MagicMock(return_value=mock_response)
         with runner.isolated_filesystem() as fs:
-            with mock.patch('builtins.open', m), mock.patch('requests.get', r):
+            p = test.utils.conditionally_mock_open(fs, read_data=data)
+            with mock.patch('builtins.open', p), mock.patch('requests.get', r):
                 result = runner.invoke(
                         tut.interface.cli, ['new', 'project_name', '--real'])
             assert result
             assert os.listdir(fs) == ['project_name']
             files = {'pyvenv.cfg', 'bin', 'lib', 'lib64', 
                     'include',  # venv files
-                    '.git',  # git directory
+                    '.git', '.gitignore',  # git directory
                     'test',  # test directory
-                    'requirements.txt'}  # requirements file
+                    'requirements.txt',
+                    '.tutconfig.local'}  # requirements file
                     # normally there would also be
-                    # .tutconfig.local
-                    # .gitignore
                     # in addition to the above files
 
             # because we are mocking the open calls in the calls to the tool,
             # we need to check that .gitignore was created via some other
             # method.
-
-            # this is hell, the comments tell us what we're checking,
-            # and we aren't checking root_dir because its an absolute dir in
-            # a tempfile, so its not worth it.
-            # with open(root_dir/.gitignore, 'w') as f:
-            assert m.mock_calls[4][1][1] == 'w'
-            # f.write('mock_gitignore_data')
-            assert m.mock_calls[6][1][0] == 'mock_gitignore_data'
-            # ...
-            # with open(root_dir/.gitignore, 'a') as f:
-            assert m.mock_calls[8][1][1] == 'a'
-            # f.write(additional_ignored_files)
-            assert m.mock_calls[10][1][0] == ''
-            # with open(root_dir/.tutconfig.local, 'w') as f:
-            assert m.mock_calls[12][1][1] == 'w'
-            # toml.loads(local_config)
-            local_config = toml.loads(m.mock_calls[15][1][0])
+            
+            fs_files = set(os.listdir(os.path.join(fs, 'project_name')))
+            assert files == fs_files
+            with open(os.path.join(fs, 'project_name/.tutconfig.local')) as f:
+                local_config = toml.load(f)
             assert local_config['python']['version'] > 3
             assert local_config['git']['ignored-files'] == []
             # this is actually an important check, since it is one of the
@@ -60,8 +49,6 @@ class TutCLITest(unittest.TestCase):
                     'vcs': 'git', 'env': 'venv', 'lang': 'python',
                     'dep': 'requirements', 'test': 'pytest'
                     }
-            fs_files = set(os.listdir(os.path.join(fs, 'project_name')))
-            assert files == fs_files
             with open(os.path.join(fs, 'project_name/requirements.txt')) as f:
                 assert 'pytest' in f.read()
 
---
+++ test/utils.py
@@ -0,0 +1,18 @@
+from unittest.mock import MagicMock
+import _io
+_old_open = open
+
+def conditionally_mock_open(path, read_data=None):
+    # stolen from mock.patch
+    file_spec = list(set(dir(_io.TextIOWrapper)).union(set(dir(_io.BytesIO))))
+    handle = MagicMock(spec=file_spec)
+    handle.write.return_value = None
+    handle.__enter__.return_value = handle
+    handle.read.return_value = read_data
+    def patched_open(name, mode='r'):
+        if path in name:
+            return _old_open(name, mode)
+        else:
+            return handle
+
+    return patched_open
